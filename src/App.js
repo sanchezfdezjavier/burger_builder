3c196f8da2727f5c7b1bb7f6999745d57d594edd
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2019-12-08 19:35:36 +0100

    Course lecture 161(included)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<p>This is just a silly text for texting prettier </p>
+				<Layout>
+					<BurgerBuilder />
+				</Layout>
 			</div>
 		);
 	}
