f37aa1295af83082cbc039540ac4066db3ba548d
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2019-12-08 12:16:24 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<p>This is just a silly text for texting prettier </p>
+			</div>
+		);
+	}
+}
+
+export default App;
