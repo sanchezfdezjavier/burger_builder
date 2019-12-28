e32ee0f72722236b545740713d0bdc2d45928e16
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2019-12-28 11:49:39 +0100

    164 Done, remove button working safely and disabled when reached minimum ingredient quantity

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,11 +34,38 @@ export default class BurgerBuilder extends Component {
 		this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
 	};
 
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		if (oldCount <= 0) {
+			return;
+		}
+		const updatedCount = oldCount - 1;
+		const udatedIngredients = {
+			...this.state.ingredients
+		};
+		udatedIngredients[type] = updatedCount;
+		const priceDeduction = INGREDIENT_PRICES[type];
+		const oldPrice = this.state.totalPrice;
+		const newPrice = oldPrice + priceDeduction;
+		this.setState({ totalPrice: newPrice, ingredients: udatedIngredients });
+	};
+
 	render() {
+		const disabledInfo = {
+			...this.state.ingredients
+		};
+		//{salad: true, meat: false, bacon:false...}
+		for (let key in disabledInfo) {
+			disabledInfo[key] = disabledInfo[key] <= 0;
+		}
 		return (
 			<Aux>
 				<Burger ingredients={this.state.ingredients} />
-				<BuildControls ingredientAdded={this.addIngredientHandler} />
+				<BuildControls
+					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disabledInfo}
+				/>
 			</Aux>
 		);
 	}
